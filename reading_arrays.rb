8eca2db340406d5dfd6de5d287080e223d1c3a0d
Taylor Otey <taylorotey5@gmail.com>
2018-08-08 16:35:24 +0000

    automatically backed up by learn

--- reading_arrays.rb
+++ reading_arrays.rb
@@ -24,7 +24,7 @@ end
 
 def first_student_by_method
   # Write a solution that returns the first student using the built in .first method
-  Adele.first
+  return STUDENT_NAMES.first
 end
 
 def last_student_by_method
